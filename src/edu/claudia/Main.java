f72a735ac8477f12e78158b0c8ee8b366ae87843
claudiabr14 <cbenriv1305@g.educaand.es>
2025-11-19 09:22:53 +0100

    Create Main.java

---
+++ src/edu/claudia/Main.java
@@ -0,0 +1,22 @@
+package edu.claudia.ramas;
+
+import java.util.Random;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        // Lotería Primitiva: 5 números principales (1..49) + 1 complementario
+        HashSet<Integer> bolas = new HashSet<>();
+        Random r = new Random();
+        while (bolas.size() < 5) {
+            bolas.add(r.nextInt(49) + 1);
+        }
+        System.out.println("Números principales: " + bolas);
+        int complementario = r.nextInt(49) + 1;
+        System.out.println("Complementario: " + complementario);
+        int suma = bolas.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Suma de principales: " + suma);
+        System.out.println("¡Suerte en el sorteo, a ganar!!");
+        System.out.println("Cambio realizado desde GitHub");
+    }
+}
